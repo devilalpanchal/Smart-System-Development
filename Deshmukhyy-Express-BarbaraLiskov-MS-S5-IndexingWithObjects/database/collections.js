de0b1d5448690b2d1ab7c124295396d7bd4d78a3
Devilal Panchal <144801878+devilalpanchal@users.noreply.github.com>
2024-02-09 11:09:05 +0530

    Update collections.js

--- Deshmukhyy-Express-BarbaraLiskov-MS-S5-IndexingWithObjects/database/collections.js
+++ Deshmukhyy-Express-BarbaraLiskov-MS-S5-IndexingWithObjects/database/collections.js
@@ -2,3 +2,11 @@ module.exports = {
     USERS: 'users',
     MOVIEJJ: 'moviejj'
 }
+// This code is for your config.json file add add fun your file in mongo DB
+// {
+//     "port": 3000,
+//     "env": "development",
+//     "mongoUri": "mongodb+srv://dttechie49:bOo6h7Ee1cJsAPJs@techiecluster.al8bjgm.mongodb.net",
+//     "database": "backend-basic",
+//     "cookieSecret": "QWRtaW4-iLCJJXIiOi-JJc3N1ZXIiL-CJVc2Vybm"
+// }
